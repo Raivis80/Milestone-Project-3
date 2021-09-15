88952b4ae675a7a499c49254f675923651f0e9cd
Raivis80 <rapet80@gmail.com>
2021-09-15 11:26:05 +0000

    add login register html pages | connect to DB | Enable user authenticaion

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,18 @@
-from app import app
-from flask import Flask, render_template
+import os
+from app import app, mongo
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return render_template('index.html')
+    users = mongo.db.users.find()
+    return render_template('index.html', users=users)
 
 
 @app.route('/gallery')
@@ -11,7 +20,44 @@ def galery():
     return render_template('gallery.html')
 
 
-@app.route('/user')
-def user():
-    return render_template('user.html')
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        else:
+            register = {
+                "username": request.form.get("username").lower(),
+                "password": generate_password_hash(request.form.get("password"))
+            }
+            mongo.db.users.insert_one(register)
+            flash("User registered successfuly")
+
+
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            return redirect(url_for(
+                "profile", username=session["user"]))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
--- run.py
+++ run.py
@@ -1,1 +1,6 @@
+import os
 from app import app
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")))
